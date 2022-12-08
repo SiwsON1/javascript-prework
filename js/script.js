1e8e1eefe58fcf10a7ffd7e45d7c2f992389b3aa
Marcin Siwonia <mahinek12@gmail.com>
2022-12-08 20:43:04 +0100

    Add minor js improvment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-function playGame(playerInput){
+{
+const playGame = function(playerInput){
   clearMessages()
 
-    function getMoveName(argMoveId){
+    const getMoveName = function(argMoveId){
       if(argMoveId == 1){
         return 'kamień';
       } else if(argMoveId == 2) {
@@ -13,7 +14,7 @@ function playGame(playerInput){
         return 'nieznany ruch';
       }
     }
-    function displayResult(argComputerMove, argPlayerMove){
+     const displayResult = function(argComputerMove, argPlayerMove){
       console.log('moves:', argComputerMove, argPlayerMove);
       if(argComputerMove == 'kamień' && argPlayerMove== 'papier'){
         return 'Ty wygrywasz!'
@@ -39,28 +40,16 @@ function playGame(playerInput){
 
     }
 
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('Wylosowana liczba to: ' + randomNumber);
-
-    let ComputerMove = getMoveName(randomNumber);
-
-
+    const ComputerMove = getMoveName(randomNumber);
     printMessage('Mój ruch to: ' + ComputerMove);
 
-
-   /* let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    */
-
     console.log('Gracz wpisał: ' + playerInput);
-
-    let PlayerMove = getMoveName(playerInput);
-
-
+    const PlayerMove = getMoveName(playerInput);
     printMessage('Twój ruch to: ' + PlayerMove);
-
-    let gameScore = displayResult(ComputerMove, PlayerMove);
-
+    
+    const gameScore = displayResult(ComputerMove, PlayerMove);
     printMessage('Wynik to: ' + gameScore);
 
   }
@@ -74,3 +63,5 @@ function playGame(playerInput){
   document.getElementById('play-scissors').addEventListener('click', function(){
     playGame(3);
   });
+
+}
